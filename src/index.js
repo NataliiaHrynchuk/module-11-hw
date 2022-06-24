06df122b1e3cc09e9b8ec0704860557c627cfae9
Nataliia Hrynchuk <92783918+NataliiaHrynchuk@users.noreply.github.com>
2022-06-24 15:51:23 +0300

    update

---
+++ src/index.js
@@ -0,0 +1,31 @@
+const BASE_URL = "https://pixabay.com/api/";
+const API_KEY = "28247101-24b63c3c82da89bc4099ab93b";
+// const options = {
+//    headers: {
+//     key: API_KEY,
+// },
+// }
+let searchQuery = '';
+const url = `${BASE_URL}/?key=${API_KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true`
+const refs = {
+    form: document.querySelector('#search-form'),
+};
+
+function onSearch(event) {
+    event.preventDefault();
+    searchQuery = event.currentTarget.elements.searchQuery.value;
+    console.log(searchQuery);
+    return fetch(url)
+    .then(response => {
+        if(!response.ok) {
+            throw new Error (response.statusText);
+        }
+        return response.json();
+    })
+    .catch(error => {
+        console.log(error);
+    })
+       
+}
+
+refs.form.addEventListener('submit', onSearch);
