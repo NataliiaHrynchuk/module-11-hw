53ea012c278ed638059e18150e07ec87f137ec23
Nataliia Hrynchuk <92783918+NataliiaHrynchuk@users.noreply.github.com>
2022-07-02 22:41:49 +0300

    add async/await

--- src/fetchImages.js
+++ src/fetchImages.js
@@ -13,8 +13,9 @@ const API_KEY = "28247101-24b63c3c82da89bc4099ab93b";
 
 export async function fetchImages(searchQuery, page) {
     try {
-        let response = await axios.get(`${BASE_URL}/?key=${API_KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`);
-        const {data} = await response.data;
+        const response = await axios.get(`${BASE_URL}/?key=${API_KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`);
+        return response.data;
+        
     } catch(error) {
     console.log(error);
 }
