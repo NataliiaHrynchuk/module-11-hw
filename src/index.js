b2118d8347e7d53b8fb1d5dc5d94bd2b7c978eb9
Nataliia Hrynchuk <natagrinchukk@gmail.com>
2022-07-04 17:05:17 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -20,8 +20,29 @@ let gallery = new SimpleLightbox('.gallery a', {
 });
 
 
+// const renderImagesList = (webformatURL, largeImageURL, tags, likes, views, comments, downloads) => {
+//     return `<div class="photo-card">
+//     <a class="gallery__item" href="${largeImageURL}">
+//     <img class="gallery__image" src="${webformatURL}" alt="${tags}" loading="lazy" />
+//     </a>
+//     <div class="info">
+//       <p class="info-item">
+//         <b>Likes</b> ${likes}
+//       </p>
+//       <p class="info-item">
+//         <b>Views</b> ${views}
+//       </p>
+//       <p class="info-item">
+//         <b>Comments</b> ${comments}
+//       </p>
+//       <p class="info-item">
+//         <b>Downloads</b> ${downloads}
+//       </p>
+//     </div>
+// </div>`
+// }
 const renderImagesList = (webformatURL, largeImageURL, tags, likes, views, comments, downloads) => {
-    return `<div class="photo-card">
+        return `<div class="photo-card">
     <a class="gallery__item" href="${largeImageURL}">
     <img class="gallery__image" src="${webformatURL}" alt="${tags}" loading="lazy" />
     </a>
@@ -40,40 +61,44 @@ const renderImagesList = (webformatURL, largeImageURL, tags, likes, views, comme
       </p>
     </div>
 </div>`
-}
+    };
 
 function onSearch(event) {
     event.preventDefault();
     searchQuery = event.currentTarget.elements.searchQuery.value;
     console.log(searchQuery);
     fetchImages(searchQuery, page)    
-    .then((data) => {
-        // console.log(data.hits);
-        console.log(`totalHits: ${data.totalHits}`);
-        const {hits} = data;
-        hits.map(hit =>{
-            const {
-                webformatURL,
-                largeImageURL,
-                tags, 
-                likes,
-                views, 
-                comments,
-                downloads
-            } = hit;
-        console.log(`webformatURL: ${webformatURL}, largeImageURL: ${largeImageURL}, tags: ${tags}, likes: ${likes}, views: ${views}, comments: ${comments}, downloads: ${downloads}`);
-        refs.imageContainer.insertAdjacentHTML("beforeend", renderImagesList(webformatURL, largeImageURL, tags, likes, views, comments, downloads));
-        
-    });
+        .then((data) => {
+            // console.log(data.hits);
+            console.log(`totalHits: ${data.totalHits}`);
+            const { hits } = data;
+            // return hits;
+       
+            hits.map(hit => {
+                const {
+                    webformatURL,
+                    largeImageURL,
+                    tags,
+                    likes,
+                    views,
+                    comments,
+                    downloads
+                } = hit;
+                console.log(`webformatURL: ${webformatURL}, largeImageURL: ${largeImageURL}, tags: ${tags}, likes: ${likes}, views: ${views}, comments: ${comments}, downloads: ${downloads}`);
+                refs.imageContainer.insertAdjacentHTML("beforeend", renderImagesList(webformatURL, largeImageURL, tags, likes, views, comments, downloads));
+            
+        })
+           
+        })
     gallery.on('show.SimpleLightbox', function (e) {
         sourceAttr: 'href';
     });
-        page +=1;
+        // page +=1;
         refs.loadMoreBtn.classList.remove("is-hidden");
-    })
     
-       
-}
+    
+     } 
+
 
 function onLoadMore(event) {
     event.preventDefault();
