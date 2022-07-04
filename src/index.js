c411cbc04c344ba4bb9d1cb5ce9dcfd18099020c
Nataliia Hrynchuk <natagrinchukk@gmail.com>
2022-07-04 15:07:07 +0300

    add styles

--- src/index.js
+++ src/index.js
@@ -27,16 +27,16 @@ const renderImagesList = (webformatURL, largeImageURL, tags, likes, views, comme
     </a>
     <div class="info">
       <p class="info-item">
-        <b>Likes ${likes}</b>
+        <b>Likes</b> ${likes}
       </p>
       <p class="info-item">
-        <b>Views ${views}</b>
+        <b>Views</b> ${views}
       </p>
       <p class="info-item">
-        <b>Comments ${comments}</b>
+        <b>Comments</b> ${comments}
       </p>
       <p class="info-item">
-        <b>Downloads ${downloads}</b>
+        <b>Downloads</b> ${downloads}
       </p>
     </div>
 </div>`
